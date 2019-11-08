f8d9937881781edcd1ad2810121e9637102cb401
Dmitry <unicornum.verum@gmail.com>
2019-11-08 23:48:19 +0300

    [VisualStudioBugs] Vs2019.

--- ErrorGetRecipeFile.cmd
+++ ErrorGetRecipeFile.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set MSBuild="%ProgramFiles% (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+set MSBuild="%ProgramFiles% (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
 
 %MSBuild% ErrorGetRecipeFile.sln /t:BuildAll:Rebuild /p:Configuration=Debug;Platform=x64
 
