9f2ef0ad8e02abe5b183f74596e1d6b550878507
Dmitry <unicornum.verum@gmail.com>
2019-12-13 19:26:42 +0300

    [VisualStudioBugs] SehExceptionCpp17.

---
+++ SehExceptionCpp17/SehExceptionCpp17.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <src/gtest-all.cc>
+#include <src/gmock-all.cc>
+
+class Check_test :
+  public ::testing::Test
+{
+};
+
+TEST_F(Check_test, /*DISABLED_*/Test_CheckExpect)
+{
+  class Proxy
+  {
+  public:
+    MOCK_METHOD0(ExpectFailed, void(void));
+  };
+
+  Proxy CheckProxy;
+
+  using namespace ::testing;
+
+  EXPECT_CALL(CheckProxy, ExpectFailed())
+    .Times(1);
+
+  CheckProxy.ExpectFailed();
+}
+
+int main()
+{
+  ::testing::InitGoogleMock();
+
+  return RUN_ALL_TESTS();
+}
