0264efee4d2a4d28624eb3612e11c3de44d17aee
Dmitry <unicornum.verum@gmail.com>
2020-05-14 17:37:57 +0300

    [VisualStudioBugs] Добавлен проект IgnoreOutputMessage.

---
+++ IgnoreOutputMessage/IgnoreOutputMessage.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+#define STRING2(x) #x
+#define STRING(x) STRING2(x)
+
+int main()
+{
+  __pragma(message(__FILE__ "(" STRING(__LINE__) "): message : выполняется компиляция ..."))
+
+  std::cout << "Hello World!\n";
+}
