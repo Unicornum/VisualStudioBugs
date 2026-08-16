636c6ec0f55588c314ede7abf6bc33b257ad68c8
unicornum.verum <unicornum.verum@gmail.com>
2026-08-16 13:40:03 +0400

    [VisualStudioBugs] Перенос кода на VS2026.

--- ErrorGetRecipeFile.cmd
+++ ErrorGetRecipeFile.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set MSBuild="D:\Program Files\Visual Studio 2022\MSBuild\Current\Bin\amd64\msbuild.exe"
+set MSBuild="C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\amd64\msbuild.exe"
 
 %MSBuild% ErrorGetRecipeFile.sln /t:BuildAll:Rebuild /p:Configuration=Debug;Platform=x64
 
